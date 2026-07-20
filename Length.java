e7b299ee778ca5327419fe7a1d4226fe08a2fa25
priankaselvadoss-web <priankaselvadoss@gmail.com>
2026-07-20 10:57:57 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,7 @@
+class Length {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] words = s.split(" ");
+        return words[words.length - 1].length();
+    } 
+}
