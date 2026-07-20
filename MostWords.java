29a15fdac1c407b6a31f10d5100246ec0e4c1c8f
priankaselvadoss-web <priankaselvadoss@gmail.com>
2026-07-20 11:21:10 +0530

    Add files via upload

---
+++ MostWords.java
@@ -0,0 +1,11 @@
+class MostWords {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+
+        for (String sentence : sentences) {
+            max = Math.max(max, sentence.split(" ").length);
+        }
+
+        return max;
+    }
+}
