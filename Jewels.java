e229da6095f6de30ad21b80c16d696cf398e029f
priankaselvadoss-web <priankaselvadoss@gmail.com>
2026-07-22 20:40:59 +0530

    Add files via upload

---
+++ Jewels.java
@@ -0,0 +1,11 @@
+class Jewels {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(char stone: stones.toCharArray()){
+            if(jewels.indexOf(stone) != -1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
